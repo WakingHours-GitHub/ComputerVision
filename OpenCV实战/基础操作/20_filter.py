09e2942fc1a69c2bb4d30a65ddb7144cc3738e1e
WakingHours-GitHub <WakingHoursHUC@outlook.com>
2022-09-06 18:13:35 +0800

    9.6

--- OpenCV实战/基础操作/20_filter.py
+++ OpenCV实战/基础操作/20_filter.py
@@ -31,5 +31,84 @@ def filter2D() -> None:
     cv.destroyAllWindows()
 
 
+def boxFilter() -> None:
+    img = cv.imread("test.jpg")
+
+    # 方盒卷积
+    dst1 = cv.boxFilter(
+        src=img,
+        ddepth=-1,  # -1表示位深和原图相同.
+        ksize=(3, 3),  # 注意, 这里ksize使用的是tuple类型, 也就是需要传入长宽才可以
+        normalize=True,  # a = 1/h*w, 退化成均值滤波
+    )
+    dst2 = cv.boxFilter(
+        img,
+        -1,
+        (3, 3),  #
+        normalize=False  # a=1, 这样就变成了真正的放box Filter了
+    )
+
+    # show image
+    cv.imshow("img", img)
+    cv.imshow("dst1", dst1)
+    cv.imshow("dst2", dst2)
+
+    cv.waitKey(0)  #
+
+    cv.destroyAllWindows()
+
+
+def blur_filter() -> None:
+    img = cv.imread("test.jpg")
+
+    # use blur filter
+    dst = cv.blur(
+        src=img,  # src img
+        ksize=(3, 3),
+        anchor=(-1, -1) # 表示自动设置锚点
+    )
+
+    # show image
+    cv.imshow("img", img)
+
+    cv.imshow("dst", dst)
+
+    cv.waitKey(0)
+
+    cv.destroyAllWindows()
+
+
+def gaussian_filter() -> None:
+    """use gaussian filter to deal figure"""
+
+    # read image
+    img = cv.imread("test.jpg")
+
+    # use gauss filter
+    dst = cv.GaussianBlur(
+        src=img,
+        ksize=(3, 3),
+        sigmaX=1,
+        sigmaY=1,
+
+    )
+
+    # show image
+    cv.imshow("img", img)
+
+    cv.imshow("dst", dst)
+
+    cv.waitKey(0)
+
+    cv.destroyAllWindows()
+
+
+
+
+
+
 if __name__ == '__main__':
-    filter2D()
+    # filter2D() # 卷积
+    # boxFilter() # 方盒滤波
+    # blur_filter() # 均值滤波
+    gaussian_filter() # 高斯滤波
