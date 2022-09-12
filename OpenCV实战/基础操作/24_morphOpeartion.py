7c217d0a350183dbe0709a0115c1631de0963235
WakingHours-GitHub <WakingHoursHUC@outlook.com>
2022-09-12 16:51:10 +0800

    9.12

--- OpenCV实战/基础操作/24_morphOpeartion.py
+++ OpenCV实战/基础操作/24_morphOpeartion.py
@@ -1,10 +1,99 @@
 """
 形态学操作:
     开运算: 先腐蚀后膨胀, 用于消除图片外面的噪点. 
-    闭运算
-    顶帽
+    闭运算: 先膨胀后腐蚀, 用于消除中间区域内部的噪点.
+    形态学梯度: 原图-腐蚀, 得到的就是边缘部分, 不过卷积核的选择是非常重要的.
+    顶帽:
     黑帽
 
 
 
 """
+
+import cv2 as cv
+import numpy as np
+
+
+
+def open_operation():
+    """
+    开运算: 先腐蚀后膨胀
+    :return:
+    """
+    # read image:
+    img = cv.imread("dot_i.png") # read carry dot image
+    img_gray = cv.cvtColor(
+        img,
+        cv.COLOR_BGR2GRAY
+    )
+    # image binary
+    ret, img_bin = cv.threshold(
+        img_gray,
+        180,
+        255,
+        cv.THRESH_BINARY
+    )
+
+    # get kernel
+    kernel = cv.getStructuringElement(cv.MORPH_RECT, ksize=(7, 7))
+
+    # morphology exchange: Open operation
+    img_open = cv.morphologyEx(
+        src=img_bin, # 使用经过二值化之后的图片,
+        op=cv.MORPH_OPEN,  # 要执行的操作, opencv定义了几个标识符. 用来表示不同的操作.
+        kernel=kernel, # kernel可以自己创建, 也可以直接使用opencv提供过的api
+
+    )
+
+    # show image
+    cv.imshow("img", np.hstack([img_gray, img_bin, img_open]))
+    # 可见, 最左边是带有噪点的原图, 可以见到比较模糊. 然后经过二值化后, 图像变得清晰起来, 这是因为我们使用
+    # threshold进行二值化了.  然后经过开运算后, 去除图像外部的噪点.
+    # 如果噪点比较大可以选择比较大的kernel, 不过这可能导致原图腐蚀过大, 导致原图无法膨胀恢复,
+    # 不过我们设置iterations, 进行多次开运算.
+
+    cv.waitKey(0)
+
+    cv.destroyAllWindows()
+
+def close_operation():
+    """
+    闭运算: 先膨胀后腐蚀, 用于去除图像内部的噪点.
+    :return:
+    """
+
+    # read image
+    img_gray = cv.imread("dot_in_i.png", cv.IMREAD_GRAYSCALE) # 直接读取灰度图
+
+    # image binary
+    ret, img_bin = cv.threshold(img_gray, 130, 255 , cv.THRESH_BINARY)
+
+    # get kernel
+    kernel = cv.getStructuringElement(cv.MORPH_RECT, (11, 11)) # 这里ksize是可以调整的, 取决于闭运算效果
+    # 开运算:
+    img_close = cv.morphologyEx(img_bin, cv.MORPH_CLOSE, kernel)
+
+    # show image
+    cv.imshow("img", np.hstack([img_bin, img_close]))
+    # 可见, 从左到右依次是二值化图, 以及经过闭运算后的图形.
+    # 可见, 闭运算是去除黑底白色物体中, 白色物体的黑色噪点
+
+    cv.waitKey(0)
+
+    cv.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    # open_operation()
+    close_operation()
